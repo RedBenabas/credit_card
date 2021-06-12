20e921fc1c6c5e5340e3c79a365dca81df156384
“Red <rbenabas@mdsol.com>
2021-06-12 13:47:52 +0100

    initial project created using sprintboot initializer

---
+++ src/test/java/com/publicis/credit_card/CreditCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicis.credit_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
